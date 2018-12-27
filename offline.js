162a721fdade70a32ea902073692f5044c3e96eb
Daniell Mesquita <daniellplasmmer@gmail.com>
2018-12-26 22:34:05 -0200

    Initial commit
    Signed-off-by: Daniell Mesquita <daniellplasmmer@gmail.com>

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1540392167,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/canvas.png",
+		"images/status.png",
+		"images/rubber-sheet0.png",
+		"images/rubber-sheet1.png",
+		"images/canvaspen-sheet0.png",
+		"images/canvaspen-sheet1.png",
+		"images/clear-sheet0.png",
+		"images/canvascolor-sheet0.png",
+		"images/color-sheet0.png",
+		"images/color-sheet1.png",
+		"images/pen_crsr-sheet0.png",
+		"images/cpen-sheet0.png",
+		"images/cpen-sheet1.png",
+		"images/rubber_crsr-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
